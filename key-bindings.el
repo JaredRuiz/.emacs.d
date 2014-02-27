6c6358700260b2b889c661796ac2093ef1cd9b20
JaredRuiz <jared@semcat.net>
2014-02-27 10:53:46 -0600

    cannot seperate C-m from RET without fudging stuff up

--- key-bindings.el
+++ key-bindings.el
@@ -1,9 +1,10 @@
 ;; keybindings.el
 
 ;; RET just returns; C-m doesn't do anything
-(global-unset-key (kbd "C-m"))
-(global-unset-key (kbd "<return>"))
-(define-key global-map (kbd "<return>") 'newline)
+;; this is still fudging things up...
+;; (global-unset-key (kbd "C-m"))
+;; (global-unset-key (kbd "<return>"))
+;; (define-key global-map (kbd "<return>") 'newline)
 
 ;; Quickly hop between brackets of functions
 (define-key global-map (kbd "C-(") 'backward-sexp)
@@ -19,9 +20,9 @@
 (define-key global-map (kbd "C-/") 'comment-or-uncomment-region-or-line)
 
 ;; Jumps to first word in line, skipping any spaces or comments
-(define-key global-map (kbd "C-m C-a") 'fast-line-first-word)
+(define-key global-map (kbd "C-c C-a") 'fast-line-first-word)
 ;; Jumps to last word in line, skipping any spaces or comments
-(define-key global-map (kbd "C-m C-e") 'fast-line-last-word)
+(define-key global-map (kbd "C-c C-e") 'fast-line-last-word)
 
 
 
