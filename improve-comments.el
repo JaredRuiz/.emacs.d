a87256e58bb24d72cc5d42719c1481f47a346120
JaredRuiz <jared@semcat.net>
2014-04-17 11:38:46 -0500

    improves improve-comments

--- improve-comments.el
+++ improve-comments.el
@@ -6,9 +6,31 @@
     (interactive)
     (let (beg end)
         (if (region-active-p)
-            (setq beg (region-beginning) end (region-end))
-            (setq beg (line-beginning-position) end (line-end-position)))
-        (comment-or-uncomment-region beg end))
-    )
-  
+            (setq 
+                     beg (region-beginning) 
+                     end (region-end)
+            )
+            (
+             setq 
+             beg (line-beginning-position) 
+             end (line-end-position)
+             ) ;; setq
+          ) ;; if
+        (comment-or-uncomment-region beg end)))
+
+;; adds comments at the end of functions
+(defun insert-end-function-comment ()
+  (interactive)
+  ;; (move-end-of-line)
+  (let (beg end)
+    (setq 
+              beg (point) 
+              end (line-end-position)))
+  (comment-or-uncomment-region beg end)) 
+  ;; (insert "a string"))
+
+
+
+
+
 (provide 'improve-comments)
