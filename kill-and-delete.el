3dfcce8db31ddc031acfe5b12ad5c6225f8d942b
JaredRuiz <jared@semcat.net>
2015-01-21 09:57:10 -0600

    adds nifty function to remove DOS end of line ^M crap

--- kill-and-delete.el
+++ kill-and-delete.el
@@ -17,4 +17,11 @@
 ;;   )
 ;; (define-key global-map (kbd "C-=") 'ruthlessly-kill-line)
 
+(defun remove-dos-eol ()
+  "Do not show ^M in files containing mixed UNIX and DOS line endings."
+  (interactive)
+  (setq buffer-display-table (make-display-table))
+  (aset buffer-display-table ?\^M []))
+
 (provide 'kill-and-delete)
+
